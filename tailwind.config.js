d2b5cc9570c897b4cdccfcdce3cd3ab24c86734f
d1eshi <silva.diegof03@gmail.com>
2024-05-23 15:42:00 -0300

    [add] details to player

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./*.{html,js}'],
+  mode: 'jit',
   theme: {
     extend: {},
   },
   plugins: [],
+  purge: ['./*.html', './**/*.{js,jsx,ts,tsx,vue}'],
 }
